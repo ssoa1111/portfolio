01c50d5b0a91ec00d636372c2dd6ca42f556fefc
ssoa1111 <rishavallen@hanmail.net>
2022-04-21 11:52:01 +0900

    contents update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+////////////////////모바일 탭 메뉴 클릭 이벤트
 const menuButton = document.querySelector('.menu ');
 const menuButtonImg = document.querySelectorAll('.menu .menu__button > img');
 const menuContents = document.querySelector('.menu .menu__contents');
@@ -14,6 +15,8 @@ menuButton.addEventListener('click',()=>{
         menuContents.classList.remove('active');
     }
 });
+
+////////////////////모바일 탭 메뉴가 스크롤 내려야 보임
 window.addEventListener('scroll',
     _.throttle(function(){
         if(window.scrollY > 100){
@@ -29,7 +32,6 @@ window.addEventListener('scroll',
         }
     },300)
 );
-
 
 
 /////////////////////intro string move
@@ -75,28 +77,61 @@ clickEvent.addEventListener('mouseenter',()=>{
 });
 
 
+////////////////////pc dot menu click, scroll
 const menuSectionMove = document.querySelectorAll('.section_move');
-const menuDivMove = document.querySelector('.div_move');
 const sectionPart = document.querySelectorAll('section');
-const divPart = document.querySelector('.intro');
 menuSectionMove.forEach(function(item, index){
     item.addEventListener('click',()=>{
-        for(let k = 0; k <menuSectionMove.length; k++){
-            menuSectionMove[k].classList.remove('active');
-        }
-        menuDivMove.classList.remove('active');
-        
-        menuSectionMove[index].classList.add('active');
+
+        // 스크롤 제어 없을 때 쓰면 됨
+        // //클릭하면 전체 클래스 초기화
+        // for(let k in menuSectionMove){
+        //     menuSectionMove[k].classList.remove('active');
+        // }
+        // //클릭하면 해당 영역에 active 부여
+        // menuSectionMove[index].classList.add('active');
         window.scrollTo(0,sectionPart[index].offsetTop);
     });
+
+    window.addEventListener('scroll',()=>{
+            // index가 마지막일 때 (==9 일 때)
+            if(index == sectionPart.length-1){
+                //index가 마지막 section top보다 클 때
+                if(window.scrollY >= sectionPart[index].offsetTop){
+                    menuSectionMove[index].classList.add('active');
+                }else{
+                    menuSectionMove[index].classList.remove('active');
+                }
+            }// index가 마지막이 아닐 때 범위를 section 위 아래로 top지정
+            else{ 
+                if(window.scrollY >= sectionPart[index].offsetTop && window.scrollY < sectionPart[index+1].offsetTop){
+                    menuSectionMove[index].classList.add('active');
+                }else{
+                    menuSectionMove[index].classList.remove('active');
+                }
+            }
+        }
+    )
 });
-menuDivMove.addEventListener('click', ()=>{
-    for(let k = 0; k <menuSectionMove.length; k++){
-        menuSectionMove[k].classList.remove('active');
-    }
-    menuDivMove.classList.add('active');
-    window.scrollTo(0,divPart.offsetTop);
-})
+////////////////////mobile, tab 메뉴 클릭하면 해당 section 이동 // 모바일이라 메뉴가 5개임 //
+const mobileMenuList = document.querySelectorAll('.menu .menu__contents ul li');
+console.log(mobileMenuList);
+mobileMenuList[0].addEventListener('click',()=>{
+    window.scrollTo(0,sectionPart[0].offsetTop);
+});
+mobileMenuList[1].addEventListener('click',()=>{
+    window.scrollTo(0,sectionPart[1].offsetTop);
+});
+mobileMenuList[2].addEventListener('click',()=>{
+    window.scrollTo(0,sectionPart[3].offsetTop);
+});
+mobileMenuList[3].addEventListener('click',()=>{
+    window.scrollTo(0,sectionPart[6].offsetTop);
+});
+mobileMenuList[4].addEventListener('click',()=>{
+    window.scrollTo(0,sectionPart[9].offsetTop);
+});
+
 
 
 
@@ -104,11 +139,7 @@ menuDivMove.addEventListener('click', ()=>{
 const pcImage = document.querySelectorAll('section .contents .contents__image .tab');
 const divImage = document.querySelectorAll('section .contents .contents__image div');
 
-const timeOut = (item, num, time)=>{
-    setTimeout(()=>{
-        item.style.top = num + 'px';
-    },100*time)
-}
+
 pcImage.forEach((item, index) => {
     item.style.top = 0;
     item.addEventListener('mouseenter',()=>{
